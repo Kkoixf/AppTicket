f398958460f094507128ddde66e1c34a7c4ac8d4
Ingrid Yuri <minorinhu123@gmail.com>
2026-07-12 22:06:57 -0300

    Inserido QRcode

--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -1,18 +1,67 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { 
+  IonTabs, 
+  IonTabBar, 
+  IonTabButton, 
+  IonIcon, 
+  IonLabel, 
+  IonFab, 
+  IonFabButton, 
+  AlertController 
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { triangle, ellipse, square, qrCodeOutline } from 'ionicons/icons';
+import { BarcodeScanner, BarcodeFormat } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonFab, IonFabButton],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
-    addIcons({ triangle, ellipse, square });
+  constructor(private alertController: AlertController) {
+  
+    addIcons({ triangle, ellipse, square, qrCodeOutline });
+  }
+
+  async scanQRCode() {
+    try {
+     
+      const status = await BarcodeScanner.requestPermissions();
+      if (status.camera !== 'granted') {
+        this.showAlert('Permissão Negada', 'Precisamos de acesso à câmera para ler o QR Code.');
+        return;
+      }
+
+      // 2. Abre a câmera para escanear
+      const { barcodes } = await BarcodeScanner.scan({
+        formats: [BarcodeFormat.QrCode]
+      });
+
+     
+      if (barcodes.length > 0) {
+        const qrContent = barcodes[0].displayValue;
+        this.showAlert('Sucesso', `Código lido com sucesso: ${qrContent}`);
+        
+    
+      }
+
+    } catch (error) {
+      console.error('Erro ao ler QR Code:', error);
+    }
+  }
+
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 }
